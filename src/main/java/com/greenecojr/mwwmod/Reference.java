d4c74f43c7497e10bdb34b6886eb11e7086eff19
Kj <Kj@128.175.172.228>
2018-10-15 17:44:57 -0400

    commition onew

---
+++ src/main/java/com/greenecojr/mwwmod/Reference.java
@@ -0,0 +1,8 @@
+package com.greenecojr.mwwmod;
+
+public class Reference {
+	public static final String MODID = "mww";
+	public static final String MODNAME = "greenecojr's Minecraft World War";
+	public static final String VERSION = "Alpha 0.1";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
+}
---
+++ src/main/java/items/ItemBasic.java
@@ -0,0 +1,12 @@
+package items;
+
+import net.minecraft.item.Item;
+
+public class ItemBasic extends Item {
+	
+	public ItemBasic(String name) {
+		setUnlocalizedName(name);
+		setRegistryName(name);
+	}
+
+}
